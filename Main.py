7acdbd1c52f5574cc19a7649bc26c5cbf06e412a
Anders fogh <martinhansen4780@gmail.com>
2020-11-24 14:09:01 +0100

    tilføjet gamecard system

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 class Start():
     def Game(self):
@@ -14,13 +15,13 @@ class Start():
             self.playerlist.append(self.playername)
             self.amount -= 1
         os.system('CLS')
-        Playermec.ScoreboardCreate(self.playerlist)
+        Scoremec.ScoreboardCreate(self.playerlist)
 
 class In_out():
     def Score():
         pass
 
-class Playermec():
+class Scoremec():#holder styr på spillernes Score
 
     def ScoreboardCreate(self,list):
         self.playerlist = list
@@ -29,31 +30,80 @@ class Playermec():
         for x in list:
             self.playerscoredict.update({self.playerlist[self.counter] : 0})
             self.counter -= 1
-        print(self.playerscoredict)
-        Playermec.Scoreboard(self.playerscoredict)
+        Scoremec.Scoreboard(self.playerscoredict)
+        Scoremec.Scoreprint()
+        Gamemec.Randompick()
         #kør det næste der skal køres herefter
 
     def Scoreboard(self, dict):#indeholder selve spillerne og deres point
-        self.accesscoreboard = dict
+        self.accesscoreboard = dict#det dict der indeholder spillere som keys og score som value
 
-    def Scoreprint(): #skal printe spillernes score
+    def Scoreprint(self): #skal printe spillernes score
+        os.system('CLS')
+        print("Scoreboardet lyder\n",Scoremec.accesscoreboard)
+
+    def ScoreAdd(self,player, amount):#skal tilføje en værdi til spillernes Scoreboard
+        self.dict = Scoremec.accesscoreboard#henter det aktuelle dict der indeholder score og players
+        self.dict[player] += amount #tilføjer score til den givne player
+        Scoremec.Scoreboard(self.dict)#opdaterer det dict der ligger i funktionen scoreboard
+        Scoremec.Scoreprint()
+
+
+class Gamemec(): #holder styr på "spillekort" og turns
+
+    def Turn(self):#skifter turn
+        try:#forsøg at gøre turncount en større
+            self.turncount += 1
+        except AttributeError:#hvis turncount ikke findes så lav turn til en værdi af 1
+            self.turncount = 1
+        Gamemec.Updateactivecards(self.turncount)
+
+
+    def Randompick(self):
+        while True:
+            Gamemec.Turn()
+            self.pick = random.randrange(1,4)
+            self.turnlength = random.randrange(5,15)
+            if self.pick == 1:
+                Play.Nofuck(self.turnlength)
+            elif self.pick == 2:
+                Play.Queen()
+            elif self.pick == 3:
+                Play.Stereotype()
+
+    def Updateactivecards(self, turn):#opdaterer kortene når der er gået en tur
         pass
 
-    def ScoreAdd(player, amount):#skal tilføje en værdi til spillernes Scoreboard
+    def Activecards(self, Cardstring, Turnint):#indeholder hvilke kort er aktive og hvor lang tid de er active i en dict
+        try:
+            self.activecardsdict.update({Cardstring : Turnint})
+        except AttributeError:
+            self.activecardsdict = {}
+        print(self.activecardsdict)
+
+    def Activecardsprint():
         pass
 
-
-class Gamemec():
-    def Randompick():
+    def Activecardsadd(self,):
         pass
 
 class Gamecards():
-    pass
+
+    def Nofuck(self,turnlength):
+        print("x")
+
+    def Queen(self):
+        print("udvælg en dronning")
+        Scoremec.ScoreAdd(input("Hvem vandt"),10)
+
+    def Stereotype(self):
+        print("z")
+
 #en class der udvælger fra en anden class
 
 Start = Start()
 In_out = In_out()
-Playermec = Playermec()
+Scoremec = Scoremec()
 Gamemec = Gamemec()
-Gamecards = Gamecards()
+Play = Gamecards()
 Start.Game()
