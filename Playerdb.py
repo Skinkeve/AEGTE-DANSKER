217f43ab0c6fd98af8f27f3eb24275a371f85b19
skinkeve <martinhansen4780@gmail.com>
2021-02-24 14:08:43 +0100

    Lavede menu errorpopup, så man kun kan starte spillet hvis man har mere end 2 spillere

--- Playerdb.py
+++ Playerdb.py
@@ -10,6 +10,8 @@ from kivy.uix.listview import ListItemButton
 from kivy.uix.popup import Popup
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.label import Label
+
 #to do:
 #lave et bedre system til at udvælge kort, måske med en lettere måde at definere chancer på
 #rykke alt in og output ind i den rigtige klasse
@@ -25,7 +27,6 @@ class MenuScreen(Screen):
 
     def submit_player(self):
         player_name = self.name_input.text #finder name_input som tekst
-        #if player_name not in player_list:
         try:
             if player_name not in self.playerlist:
                 self.player_list.adapter.data.extend([player_name]) #lav et nyt element med navn player_name?
@@ -51,59 +52,28 @@ class MenuScreen(Screen):
             #self.player_list._trigger_reset_populate() #updater listview
 
     def start_game(self):
-        sm.current = "game"
-        #self.game = GamePopup()
-        #try:
-        #    if len(self.playerlist) > 1:
-        #        self.game.open()
-                #startgame.run()
-        #except AttributeError:
+        noplayerspopup = Popup(title='Errorpopup', content=Label(text='Add more players'), size_hint=(None, None), size=(400, 400))
+        try:
+            if len(self.playerlist) < 2:
+                noplayerspopup.open()
+            else:
+                sm.current = "game"
+        except AttributeError:
+            noplayerspopup.open()
 
     def pack_pick(self):
         pass
 
 class GameScreen(Screen):
-    pass
+
+    def Nextcard(self):
+        pass
 
 class WindowManager(ScreenManager):
     pass
-"""
-class TestApp(App):
-    def build(self):
-        # Create the screen manager
-        sm = ScreenManager()
-        sm.add_widget(MenuScreen(name='menu'))
-        sm.add_widget(GameScreen(name='game'))
-        return sm
-"""
 
 class PLayerlistbtn(ListItemButton): #fungerer som en slags handler?????
     pass
-
-"""
-class PlayerDB(BoxLayout):
-    name_input = ObjectProperty()
-    player_list = ObjectProperty()
-    sm = ScreenManager()
-    sm.add_widget(MenuScreen(name='menu'))
-    sm.add_widget(GameScreen(name='game'))
-
-# jeg vil gerne have at spillere ikke kan hedde " "
-
-
-class Game(BoxLayout):
-    pass
-
-
-class PlayerdbApp(App):
-    def build(self):
-        return PlayerDB()
-
-
-class GameApp(App):#object not callable?????? måske fordi det er en app
-    def build(self):
-        return Game()# returner en anden kivy logic der starter spillet?
-"""
 
 kv = Builder.load_file("Playerdb.kv")
 
